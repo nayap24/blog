f04f60065fcbd9c53912b444945cfa5fe58729c0
Ariapa <joe@ariapa.com>
2018-05-10 14:35:33 +0000

    minor change

--- config.py
+++ config.py
@@ -2,13 +2,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    #MAIL_SERVER = 'localhost'
-    #MAIL_PORT = 8025
     #MAIL_USE_TLS = True
-    #MAIL_USERNAME = 'joe'
-    #MAIL_PASSWORD = 'abc'
     BLOG_MAIL_SUBJECT_PREFIX = '[Blog]'
-    BLOG_MAIL_SENDER = 'Blog Admin <blog@localhost>'
+    BLOG_MAIL_SENDER = 'Blog Admin <no-rely@ariapa.com>'
     BLOG_ADMIN = 'joe@ariapa.com'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BLOG_POSTS_PER_PAGE = 20
--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,4 @@
-SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://blog:thidata@localhost/blog'
+SECRET_KEY = b'\xa9\x9dZ\xc81\x0c\x05i\xff\xc9\xc8\xc4Wc\xdd\x05'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://db_user:nayap24@localhost/ariapa'
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
